fb40f550d636ae7cff40ebd8be7db55337a870f4
AdrianaPayeDelgado <84357097+AdrianaPayeDelgado@users.noreply.github.com>
2021-12-07 16:10:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let forma = document.getElementById ("forma");
+let color = document.getElementById ("color");
+let name = document.getElementById ("iname");
+
+console.log (forma.value);
+console.log (forma.value);
+
